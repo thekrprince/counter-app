a50960930199ee626eb3676b6410faafc9c19552
princekr96 <princekr700@gmail.com>
2021-06-13 11:26:24 +0530

    function created for buttons

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-let btn = document.getElementById('btn1');
-let answerValue = document.querySelector('#value');
-let count = 0;
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+const answerValue = document.querySelector('#value');
+let counter = 0;
 
-function add() {
-  console.log('clicked');
-  btn.addEventListener('click', function () {
-    count++;
-  });
+// Add counter listener
+btn1.addEventListener('click', function () {
+  counter = counter + 1;
+  answerValue.innerHTML = counter;
+  fontColorText();
+});
 
-  answerValue.textContent = count;
-  console.log(count);
-}
+// Low counter listener
+btn2.addEventListener('click', function () {
+  counter = counter - 1;
+  answerValue.innerHTML = counter;
+  fontColorText();
+});
 
-function sub() {
-  alert('In progress');
+// Reset count
+btn3.addEventListener('click', function () {
+  counter = 0;
+  answerValue.innerHTML = counter;
+});
+
+// Changing answer font conditionally
+function fontColorText() {
+  if (counter > 0) {
+    answerValue.style.color = '#c7f464';
+  } else if (counter < 0) {
+    answerValue.style.color = '#f23e02';
+  } else {
+    answerValue.style.color = '#000';
+  }
 }
